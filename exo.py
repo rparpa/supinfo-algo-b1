fc9bcba114272d126bdf49269c883d8f0c789e58
rparpail <remi.parpaillon@gmail.com>
2014-11-29 02:58:37 +0100

    Create function getNextMove
    The function request two user input, setting x & y axis for the next move

--- exo.py
+++ exo.py
@@ -21,4 +21,15 @@ def printBoard(board) :
 			printedRow += ' o ' if value else ' x '
 		print(printedRow)
 
-printBoard(createGameBoard(boardConfig['width'], boardConfig['height']))
+def getNextMove(width, height) :
+	x = input('Where to put on x axis (between 0 and %d)' % (width))
+
+	if x < 0 or x > width:
+		getNextMove(width, height)
+
+	y = input('Where to put on y axis (between 0 and %d)' % (height))
+
+	if y < 0 or x > height:
+		getNextMove(width, height)
+
+getNextMove(4, 5)
