f12a9a2c4a6660d436fd4d998e0c2cf33784fa9a
rparpail <remi.parpaillon@gmail.com>
2014-11-29 03:33:58 +0100

    Create a switchBoard function
    This function take a given x & y position, switch its value & and its neighbors

--- exo.py
+++ exo.py
@@ -3,10 +3,8 @@
 def setBoard() :
 	return {'width': input('Set board with : '), 'height': input('Set board height :')}
 
-boardConfig = setBoard()
-
 def createGameBoard(width, height) :
-	gameboard = [];
+	gameboard = []
 	for i in range(0, height) :
 		column = []
 		for i in range(0, width) :
@@ -22,14 +20,36 @@ def printBoard(board) :
 		print(printedRow)
 
 def getNextMove(width, height) :
-	x = input('Where to put on x axis (between 0 and %d)' % (width))
+	x = input('Where to put on x axis (0 >= y < %d)' % (width))
 
 	if x < 0 or x > width:
 		getNextMove(width, height)
 
-	y = input('Where to put on y axis (between 0 and %d)' % (height))
+	y = input('Where to put on y axis (0 >= y < %d)' % (height))
 
 	if y < 0 or x > height:
 		getNextMove(width, height)
 
-getNextMove(4, 5)
+	return {'x' : x, 'y' : y}
+
+def isInGameboard(x, y, gameboard) :
+	return x >= 0 and x < len(gameboard[0]) and y >= 0 and y < len(gameboard)
+
+def switchBoardValue (x, y, gameboard) :
+	neighbors = [-1, 0, 1]
+	for neighborXDif in neighbors :
+		xToTest = x + neighborXDif
+		for neighborYDif in neighbors :
+			yToTest = y + neighborYDif
+			print (neighborXDif)
+			print (neighborYDif)
+			if isInGameboard(xToTest, yToTest, gameboard) :
+				gameboard[yToTest][xToTest] = False if gameboard[yToTest][xToTest] else True
+	return gameboard
+
+boardConfig = setBoard()
+gameboard = createGameBoard(boardConfig['width'], boardConfig['height'])
+printBoard(gameboard)
+myMove = getNextMove(boardConfig['width'], boardConfig['height'])
+gameboard = switchBoardValue(myMove['x'], myMove['y'], gameboard)
+printBoard(gameboard)
